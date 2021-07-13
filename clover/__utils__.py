cd346cb788734a5880da9151cb917054a5324d49
BenWinchester <benedict.winchester@gmail.com>
2021-07-13 12:20:33 +0100

    Utility module created.

---
+++ clover/__utils__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+########################################################################################
+# __utils__.py - CLOVER utility module.
+#
+# Author: Ben Winchester
+# Date created: 13/07/2021
+# License: Open source
+########################################################################################
+"""
+__utils__.py - Utility module for CLOVER.
+
+The utility module contains functionality which is used by various scripts, modules, and
+components across CLOVER, as well as commonly-held variables to prevent dependency
+issues and increase the ease of code alterations.
+
+"""
