4e6841d2b8eb2505206f1ac7e82b294f2ddbafd5
BenWinchester <benedict.winchester@gmail.com>
2021-07-13 16:26:05 +0100

    Moved everything back up :p

--- clover/generation/diesel.py
+++ clover/generation/diesel.py
@@ -19,24 +19,31 @@ import os
 import numpy as np
 import pandas as pd
 
-from ...__utils__ import LOCATIONS_FOLDER_NAME
+from ..__utils__ import LOCATIONS_FOLDER_NAME
 
-class Diesel():
-    def __init__(self): 
+
+class Diesel:
+    def __init__(self):
         self.size = 1
-        self.location = 'Bahraich'
+        self.location = "Bahraich"
         self.CLOVER_filepath = os.getcwd()
-        self.location_filepath = os.path.join(self.CLOVER_filepath, LOCATIONS_FOLDER_NAME, self.location)
-        self.generation_filepath = os.path.join(self.location_filepath, 'Generation')
-        self.diesel_filepath = os.path.join(self.generation_filepath, 'Diesel', 'Diesel inputs.csv')
-        self.diesel_inputs = pd.read_csv(self.diesel_filepath,header=None,index_col=0).round(decimals=3)
+        self.location_filepath = os.path.join(
+            self.CLOVER_filepath, LOCATIONS_FOLDER_NAME, self.location
+        )
+        self.generation_filepath = os.path.join(self.location_filepath, "Generation")
+        self.diesel_filepath = os.path.join(
+            self.generation_filepath, "Diesel", "Diesel inputs.csv"
+        )
+        self.diesel_inputs = pd.read_csv(
+            self.diesel_filepath, header=None, index_col=0
+        ).round(decimals=3)
 
-#%%       
-#   Energy threshold, above which the generator should switch on
-    def find_deficit_threshold(self,unmet_energy,blackouts,backup_threshold):
+    #%%
+    #   Energy threshold, above which the generator should switch on
+    def find_deficit_threshold(self, unmet_energy, blackouts, backup_threshold):
         """
         Function:
-            Identifies the threshold level of energy at which the diesel backup 
+            Identifies the threshold level of energy at which the diesel backup
             generator switches on
         Inputs:
             unmet_energy        Load profile of currently unment energy
@@ -46,17 +53,19 @@ class Diesel():
             energy_threshold    Energy threshold (kWh) at which the diesel backup
                                 switches on
         """
-        blackout_percentage = np.mean(blackouts)[0]                         # Find blackout percentage
-        reliability_difference = blackout_percentage - backup_threshold     # Find difference in reliability
+        blackout_percentage = np.mean(blackouts)[0]  # Find blackout percentage
+        reliability_difference = (
+            blackout_percentage - backup_threshold
+        )  # Find difference in reliability
         percentile_threshold = 100.0 * (1.0 - reliability_difference)
         if reliability_difference > 0.0:
-            energy_threshold = np.percentile(unmet_energy,percentile_threshold)
+            energy_threshold = np.percentile(unmet_energy, percentile_threshold)
         else:
             energy_threshold = np.max(unmet_energy)[0] + 1.0
         return energy_threshold
-    
-#   Find times when load > energy threshold
-    def get_diesel_energy_and_times(self,unmet_energy,blackouts,backup_threshold):
+
+    #   Find times when load > energy threshold
+    def get_diesel_energy_and_times(self, unmet_energy, blackouts, backup_threshold):
         """
         Function:
             Calculates the times at which the diesel backup generator is used, and
@@ -69,14 +78,16 @@ class Diesel():
             diesel_energy       Profile of energy supplued by diesel backup
             diesel_times        Profile of times when generator is on (1) or off (0)
         """
-        energy_threshold = self.find_deficit_threshold(unmet_energy,blackouts,backup_threshold)
+        energy_threshold = self.find_deficit_threshold(
+            unmet_energy, blackouts, backup_threshold
+        )
         diesel_energy = (unmet_energy >= energy_threshold) * unmet_energy
         diesel_times = (unmet_energy >= energy_threshold) * 1
         diesel_times = diesel_times.astype(float)
-        return diesel_energy, diesel_times  
-    
-#   Find diesel fuel consumption
-    def get_diesel_fuel_usage(self,capacity,diesel_energy,diesel_times):
+        return diesel_energy, diesel_times
+
+    #   Find diesel fuel consumption
+    def get_diesel_fuel_usage(self, capacity, diesel_energy, diesel_times):
         """
         Function:
             Calculates the fuel usage of the diesel backup generator
@@ -87,13 +98,15 @@ class Diesel():
         Outputs:
             fuel_usage      Hourly profile of diesel fuel usage (litres)
         """
-        diesel_consumption = float(self.diesel_inputs[1]['Diesel consumption'])
-        diesel_minimum_load = float(self.diesel_inputs[1]['Diesel minimum load'])
+        diesel_consumption = float(self.diesel_inputs[1]["Diesel consumption"])
+        diesel_minimum_load = float(self.diesel_inputs[1]["Diesel minimum load"])
         capacity = float(capacity)
         load_factor = diesel_energy / capacity
         above_minimum = load_factor * (load_factor > diesel_minimum_load)
         below_minimum = diesel_minimum_load * (load_factor <= diesel_minimum_load)
-        load_factor = pd.DataFrame(above_minimum.values + below_minimum.values) * diesel_times
+        load_factor = (
+            pd.DataFrame(above_minimum.values + below_minimum.values) * diesel_times
+        )
         fuel_usage = load_factor * capacity * diesel_consumption
         fuel_usage = fuel_usage.astype(float)
-        return fuel_usage # in litres
+        return fuel_usage  # in litres
--- clover/generation/grid.py
+++ clover/generation/grid.py
@@ -20,17 +20,29 @@ import random
 
 import pandas as pd
 
-from ...__utils__ import LOCATIONS_FOLDER_NAME
+from ..__utils__ import LOCATIONS_FOLDER_NAME
 
-class Grid():
+
+class Grid:
     def __init__(self):
-        self.location = 'Bahraich'
+        self.location = "Bahraich"
         self.CLOVER_filepath = os.getcwd()
-        self.location_filepath = os.path.join(self.CLOVER_filepath, LOCATIONS_FOLDER_NAME, self.location)
-        self.location_inputs = pd.read_csv(os.path.join(self.location_filepath, 'Location Data', 'Location inputs.csv'),header=None,index_col=0)[1]
-        self.generation_filepath = os.path.join(self.location_filepath, 'Generation')
-        self.grid_inputs = pd.read_csv(os.path.join(self.generation_filepath, 'Grid inputs.csv'),index_col=0)
-#%%
+        self.location_filepath = os.path.join(
+            self.CLOVER_filepath, LOCATIONS_FOLDER_NAME, self.location
+        )
+        self.location_inputs = pd.read_csv(
+            os.path.join(
+                self.location_filepath, "Location Data", "Location inputs.csv"
+            ),
+            header=None,
+            index_col=0,
+        )[1]
+        self.generation_filepath = os.path.join(self.location_filepath, "Generation")
+        self.grid_inputs = pd.read_csv(
+            os.path.join(self.generation_filepath, "Grid inputs.csv"), index_col=0
+        )
+
+    #%%
     def get_lifetime_grid_status(self):
         """
         Function:
@@ -45,7 +57,7 @@ class Grid():
         for i in range(Grid().grid_inputs.shape[1]):
             grid_hours = pd.DataFrame(self.grid_inputs[grid_types[i]])
             grid_status = []
-            for day in range(365 * int(self.location_inputs['Years'])):
+            for day in range(365 * int(self.location_inputs["Years"])):
                 for hour in range(grid_hours.size):
                     if random.random() < grid_hours.iloc[hour].values:
                         grid_status.append(1)
@@ -53,24 +65,24 @@ class Grid():
                         grid_status.append(0)
             grid_name = grid_types[i]
             grid_times = pd.DataFrame(grid_status)
-            grid_times.to_csv(self.generation_filepath + grid_name + '_grid_status.csv')
+            grid_times.to_csv(self.generation_filepath + grid_name + "_grid_status.csv")
 
-    def change_grid_coverage(self,grid_type='bahraich', hours=12):
+    def change_grid_coverage(self, grid_type="bahraich", hours=12):
         grid_profile = self.grid_inputs[grid_type]
         baseline_hours = np.sum(grid_profile)
-        new_profile = pd.DataFrame([0]*24)
+        new_profile = pd.DataFrame([0] * 24)
         for hour in range(24):
-            m = interp1d([0,baseline_hours,24],[0,grid_profile[hour],1])
+            m = interp1d([0, baseline_hours, 24], [0, grid_profile[hour], 1])
             new_profile.iloc[hour] = m(hours).round(3)
-        new_profile.columns = [grid_type+'_'+ str(hours)]
+        new_profile.columns = [grid_type + "_" + str(hours)]
         return new_profile
-    
-    def save_grid_coverage(self,grid_type='bahraich',hours=12):
-        new_profile = self.change_grid_coverage(grid_type,hours)
-        new_profile_name = grid_type+'_'+ str(hours)
+
+    def save_grid_coverage(self, grid_type="bahraich", hours=12):
+        new_profile = self.change_grid_coverage(grid_type, hours)
+        new_profile_name = grid_type + "_" + str(hours)
         output = self.grid_inputs
         if new_profile_name in output.columns:
             output[new_profile_name] = new_profile
         else:
-            output = pd.concat([output,new_profile],axis=1)
-        output.to_csv(self.generation_filepath + 'Grid inputs.csv')
+            output = pd.concat([output, new_profile], axis=1)
+        output.to_csv(self.generation_filepath + "Grid inputs.csv")
