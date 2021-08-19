6348098c1fef96f835f66216146b33af7adc8ec1
BenWinchester <benedict.winchester@gmail.com>
2021-08-19 16:16:53 +0100

    Shifted to desalinating when there is excess energy

--- clover/argparser.py
+++ clover/argparser.py
@@ -109,9 +109,16 @@ def parse_args(args: List[Any]) -> argparse.Namespace:
     )
 
     # Simulation-specific arguments.
-    simulation_parser = parser.add_argument_group(
-        "simulation-only arguments",
+    clean_water_parser = parser.add_argument_group("clean-water-only arguments",)
+    clean_water_parser.add_argument(
+        "--num-clean-water-tanks",
+        default=0,
+        type=int,
+        help="The number of clean-water tanks to be included in the system.",
     )
+
+    # Simulation-specific arguments.
+    simulation_parser = parser.add_argument_group("simulation-only arguments",)
     simulation_parser.add_argument(
         "--simulation",
         action="store_true",
@@ -127,9 +134,7 @@ def parse_args(args: List[Any]) -> argparse.Namespace:
     )
 
     # Optimisation arguments
-    optimisation_parser = parser.add_argument_group(
-        "optimisation-only arguments",
-    )
+    optimisation_parser = parser.add_argument_group("optimisation-only arguments",)
     optimisation_parser.add_argument(
         "--optimisation",
         action="store_true",
