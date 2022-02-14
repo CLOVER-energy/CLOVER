17543c2ba8cad89cba304120b0f676b012a0a827
BenWinchester <benedict.winchester@gmail.com>
2022-02-14 15:07:54 +0000

    Removed pdb break

--- src/clover/__main__.py
+++ src/clover/__main__.py
@@ -1069,9 +1069,6 @@ def main(args: List[Any]) -> None:
 
             if parsed_args.analyse:
                 # Carry out an appraisal of the system.
-                import pdb
-
-                pdb.set_trace()
                 system_appraisal: Optional[SystemAppraisal] = appraise_system(
                     electric_yearly_load_statistics,
                     simulation.end_year,
