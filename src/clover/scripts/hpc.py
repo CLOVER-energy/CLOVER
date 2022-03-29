c92dc5fad641098dc188a6174706b28dd362d0ec
Ben Winchester <benedict.winchester@gmail.com>
2022-03-29 18:38:59 +0100

    Files created.

---
+++ src/clover/scripts/hpc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+########################################################################################
+# hpc.py - Wrapper script around CLOVER when run on Imperial College's HPC.            #
+#                                                                                      #
+# Authors: Phil Sandwell, Ben Winchester                                               #
+# Copyright: Phil Sandwell, 2022                                                       #
+# Date created: 29/03/2022                                                             #
+# License: Open source                                                                 #
+#                                                                                      #
+# For more information, please email:                                                  #
+#   benedict.winchester@gmail.com                                                      #
+#   philip.sandwell@gmail.com                                                          #
+########################################################################################
+"""
+hpc.py - The wrapper script for running CLOVER on the HPC.
+
+Imperial College London owns a series of high-performance computers. This module
+provides a wrapper around the main functionality of CLOVER to enable it to be run on
+the HPC.
+
+"""
