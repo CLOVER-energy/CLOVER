1747850fbd17282d01338978c8582ffe5fa5642a
Ben Winchester <benedict.winchester@gmail.com>
2022-03-29 18:09:38 +0100

    Conversion.py file class structure.

---
+++ src/clover/conversion/tests/test_conversion.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+########################################################################################
+# test_conversion.py - Tests for CLOVER's conversion module.                           #
+#                                                                                      #
+# Author: Ben Winchester                                                               #
+# Copyright: Ben Winchester, 2021                                                      #
+# Date created: 05/07/2021                                                             #
+# License: Open source                                                                 #
+########################################################################################
+"""
+test_conversion.py - Tests for the conversion module of CLOVER.
+
+"""
+
+import unittest
+import unittest.mock as mock
+
+from ..conversion import _parse_waste_production, Converter, MultiInputConverter, ThermalDesalinationPlant, WasteProduct
+
+
+class TestConverter(unittest.TestCase):
+    """Tests the convertor class."""
+
+
+class TestMultiInputConvertor(unittest.TestCase):
+    """Tests the convertor class."""
+
+
+class TestParseWasteProduction(unittest.TestCase):
+    """Tests the waste-production parsing method."""
+
+
+class TestThermalDesalinationPlant(unittest.TestCase):
+    """Tests the convertor class."""
+
+
+class TestWasteProduct(unittest.TestCase):
+    """Tests the convertor class."""
+
+
+if __name__ == '__main__':
+    unittest.main()
