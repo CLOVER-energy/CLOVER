8bbe6b53f0fb264c2c798c5907246220c454a500
Ben Winchester <benedict.winchester@gmail.com>
2022-03-29 18:38:59 +0100

    Files created.

---
+++ src/clover/scripts/hpc_clover.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+########################################################################################
+# hpc_clover.py - Entry point for running CLOVER on Imperial College's HPC.            #
+#                                                                                      #
+# Authors: Phil Sandwell, Ben Winchester                                               #
+# Copyright: Phil Sandwell, 2022                                                       #
+# Date created: 29/03/2022                                                             #
+# License: Open source                                                                 #
+#                                                                                      #
+# For more information, please email:                                                  #
+#   benedict.winchester@gmail.com                                                      #
+#   philip.sandwell@gmail.com                                                          #
+########################################################################################
+"""
+hpc_clover.py - The entry point for running CLOVER on the HPC.
+
+Imperial College London owns a series of high-performance computers. This module
+provides an entry point for running CLOVER across the various HPC computers.
+
+"""
+
+
