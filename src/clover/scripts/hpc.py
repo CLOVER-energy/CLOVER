48a5c695f1e56f932686555eda3848c604e778fe
Ben Winchester <benedict.winchester@gmail.com>
2022-03-30 16:04:49 +0100

    First attempt at code complete.

--- src/clover/scripts/hpc.py
+++ src/clover/scripts/hpc.py
@@ -21,13 +21,27 @@ the HPC.
 """
 
 import os
+import sys
+
+from ..__main__ import main as clover_main
+from ..__utils__ import BColours, get_logger
+from .hpc_utils import (
+    HpcOptimisation,
+    HpcSimulation,
+    parse_args_and_hpc_input_file,
+)
 
 
-__all__ = ("main")
+__all__ = "main"
 
 # HPC Job Number:
 #   Name of the environment variable for the HPC job number.
 HPC_JOB_NUMBER: str = "PBS_ARRAY_INDEX"
+
+
+# Logger name:
+#   The name to use for the logger for this script.
+LOGGER_NAME: str = "hpc_run_{}"
 
 
 def main(args) -> None:
@@ -36,12 +50,54 @@ def main(args) -> None:
 
     """
 
-    # Call the utility module to parse the HPC run information.
     # Determine the run that is to be carried out.
     hpc_job_number = os.getenv(HPC_JOB_NUMBER)
+    logger = get_logger(LOGGER_NAME.format(hpc_job_number), False)
+
+    # Call the utility module to parse the HPC run information.
+    logger.info("Parsing HPC input file.")
+    _, runs = parse_args_and_hpc_input_file(args)
+    logger.info("HPC input file successfully parsed.")
+
+    # Determine the run.
+    try:
+        run = runs[hpc_job_number]
+    except IndexError:
+        logger.error(
+            "%sRun number %s out of bounds. Only %s runs submitted.%s",
+            BColours.fail,
+            hpc_job_number,
+            len(runs),
+            BColours.endc,
+        )
+    logger.info("Run successfully determined: %s", str(run))
+
+    # Setup the arguments to pass to CLOVER.
+    clover_arguments = [
+        "--location",
+        run.location,
+    ]
+
+    if isinstance(run, HpcOptimisation):
+        logger.info("Run %s is an optimisation.", hpc_job_number)
+        clover_arguments.append("--optimisation")
+    elif isinstance(run, HpcSimulation):
+        logger.info("Run %s is a simulation.", hpc_job_number)
+        clover_arguments.extend(
+            [
+                "--simulation",
+                "--pv-system-size",
+                run.pv_system_size,
+                "--storage-size",
+                run.storage_size,
+            ]
+        )
+
+    if run.total_load:
+        clover_arguments.extend(["--electric-load-profile", run.total_load_file])
 
     # Call CLOVER with this information.
-
+    clover_main(clover_arguments)
 
 
 if __name__ == "__main__":
