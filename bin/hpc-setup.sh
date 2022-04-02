00e43f3898f78a1f4fd3610343722f465e18eca9
Ben Winchester <benedict.winchester@gmail.com>
2022-04-02 13:47:19 +0100

    Created setup scripts.

---
+++ bin/hpc-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+########################################################################################
+# hpc-setup.sh - HPC setup script for CLOVER.                                          #
+#                                                                                      #
+# Author: Ben Winchester                                                               #
+# Copyright: Ben Winchester, 2022                                                      #
+# Date created: 02/04/2022                                                             #
+#                                                                                      #
+# For more information, please email:                                                  #
+#   benedict.winchester@gmail.com                                                      #
+########################################################################################
+
