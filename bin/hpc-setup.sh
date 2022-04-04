960bc5ec489ffc22f4d85106edd36d48f4d6efe4
Ben Winchester <benedict.winchester@gmail.com>
2022-04-04 11:36:14 +0100

    Rough hpc setup script created.

--- bin/hpc-setup.sh
+++ bin/hpc-setup.sh
@@ -10,3 +10,22 @@
 #   benedict.winchester@gmail.com                                                      #
 ########################################################################################
 
+# Load the anaconda program
+module load anaconda3/personal
+
+# Create the required environment
+echo -e "Creating anaconda virtual environment .........................    "
+if source create -n "py37" python=3.7.10 ipython ; then
+    echo -e "Creating anaconda virtual environment .........................    [   DONE   ]"
+else
+    echo -e "Creating anaconda virtual environment .........................    [  FAILED  ]"
+fi
+source activate py37
+
+# Install the necessary packages.
+echo -e "Installing necessary packages .................................    "
+if python -u -m pip install -r requirements.txt ; then
+    echo -e "Installing necessary packages .................................    [   DONE   ]"
+else
+    echo -e "Installing necessary packages .................................    [  FAILED  ]"
+fi
