f0d336fd76bf36267ee0aa4445617de907fa10a8
Ben Winchester <benedict.winchester@gmail.com>
2022-08-09 11:50:15 +0100

    :construction: Working on solar tests

--- src/clover/generation/tests/unit/test_solar.py
+++ src/clover/generation/tests/unit/test_solar.py
@@ -12,13 +12,15 @@ test_solar.py - Tests for the solar generation module of CLOVER.
 
 """
 
+from tkinter import N
 import unittest
 
-from unittest import mock  # pylint: disable=unused-import
+from unittest import mock
+from clover.__utils__ import ProgrammerJudgementFault, RegressorType  # pylint: disable=unused-import
 
 import pytest
 
-from ...solar import PerformanceCurve
+from ...solar import HybridPVTPanel, PVPanel, PerformanceCurve
 
 
 class TestPerformanceCurve(unittest.TestCase):
@@ -43,3 +45,98 @@ class TestPerformanceCurve(unittest.TestCase):
         self.assertEqual(zeroth, performance_curve.eta_0)
         self.assertEqual(first, performance_curve.c_1)
         self.assertEqual(second, performance_curve.c_2)
+
+
+class TestPVPanel(unittest.TestCase):
+    """Tests the :class:`PVPanel` instance."""
+
+    def setUp(self) -> None:
+        """Sets up functionality in common across test cases."""
+        self.input_data = {
+            "name": "default_pv",
+            "azimuthal_orientation": 180,
+            "lifetime": 20,
+            "reference_efficiency": 0.125,
+            "reference_temperature": 25,
+            "thermal_coefficient": 0.0053,
+            "tilt": 29,
+            "type": "pv",
+            "costs": {
+                "cost": 500,
+                "cost_decrease": 5,
+                "installation_cost": 100,
+                "installation_cost_decrease": 0,
+                "o&m": 5
+            },
+            "emissions":{
+            "ghgs": 3000,
+            "ghg_decrease": 5,
+            "installation_ghgs": 50,
+            "installation_ghg_decrease": 0,
+            "o&m": 5}
+        }
+        super().setUp()
+
+    @pytest.mark.unit
+    def test_instantiate_no_override(self) -> None:
+        """Tests instantiation with the default PV unit."""
+
+        PVPanel.from_dict(mock.MagicMock(), self.input_data)
+
+    @pytest.mark.unit
+    def test_instantiate_override_unit(self) -> None:
+        """
+        Tests instantiation with an overriden PV unit."""
+
+        overriden_unit: float = 0.5
+        self.input_data["pv_unit"] = overriden_unit
+        pv_panel = PVPanel.from_dict(mock.MagicMock(), self.input_data)
+
+        self.assertEqual(overriden_unit, pv_panel.pv_unit)
+        self.assertTrue(pv_panel.pv_unit_overrided)
+
+    @pytest.mark.unit
+    def test_calculate_performance(self) -> None:
+        """Tests the calculate performance method."""
+
+        pv_panel = PVPanel.from_dict(mock.MagicMock(), self.input_data)
+        with self.assertRaises(ProgrammerJudgementFault):
+            pv_panel.calculate_performance(0, 0, 0, 0, 0, 0, 0)
+
+
+class TestHybridPVTPanelPerformance(unittest.TestCase):
+    """Tests the `calculate_performance` function of the hybrid PV-T panel."""
+
+
+    def setUp(self) -> None:
+        """Sets up functionality in common across test cases."""
+        self.input_data = {
+            "name": "default_pvt",
+            "azimuthal_orientation": 180,
+            "lifetime": 20,
+            "max_mass_flow_rate": 7.37,
+            "min_mass_flow_rate": 7.37,
+            "pv": "default_pv",
+            "pv_unit": 0.2,
+            "reference_efficiency": 0.125,
+            "reference_temperature": 25,
+            "thermal_coefficient": 0.0053,
+            "tilt": 29,
+            "type": "pv_t",
+            "costs": {
+                "cost": 500,
+                "cost_decrease": 5,
+                "installation_cost": 100,
+                "installation_cost_decrease": 0,
+                "o&m": 5
+            },
+            "emissions":{
+            "ghgs": 3000,
+            "ghg_decrease": 5,
+            "installation_ghgs": 50,
+            "installation_ghg_decrease": 0,
+            "o&m": 5}
+        }
+        super().setUp()
+
+    
