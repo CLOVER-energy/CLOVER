3a7b566e42eb22864c1aad59f1767a8049828a1c
BenWinchester <benedict.winchester@gmail.com>
2023-07-11 17:52:09 +0100

    Update script

--- bin/test-clover.sh
+++ bin/test-clover.sh
@@ -8,18 +8,18 @@
 
 echo "Running test suite: black, mypy, pylint and pytest."
 echo -e "Running black...\e[0m"
-python3.7 -m black src/clover --check --diff
+python -m black src/clover --check --diff
 echo -e "\e[1mBlack formatter done, see above for details.\e[0m"
 echo -e "\e[1mRunning mypy...\e[0m"
-python3.7 -m mypy src/clover
+python -m mypy src/clover
 echo -e "\e[1mMypy done, see above for details.\e[0m"
 echo -e "\e[1mRunning pylint...\e[0m"
-python3.7 -m pylint src/clover
+python -m pylint src/clover
 echo -e "\e[1mPylint done, see above for details.\e[0m"
 echo -e "\e[1mRunning yamllint...\e[0m"
 yamllint -c .yamllint-config.yaml locations/Bahraich
 yamllint -c .yamllint-config.yaml src/
 echo -e "\e[1mYamllint done, see above for details.\e[0m"
 echo -e "\e[1mRunning pytest...\e[0m"
-python3.7 -m pytest src/clover
+python -m pytest src/clover
 echo -e "\e[1mTest suite complete: see above stdout for details.\e[0m"
